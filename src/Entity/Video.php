71ed74fa42a1e4c51ddaf1228f541e6357f8ab08
samantha-eva <samanthathiebaut@gmail.com>
2025-09-02 22:50:18 +0200

    Relation order et video

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -34,9 +34,16 @@ class Video
     #[ORM\JoinColumn(nullable: false)]
     private ?Categorie $categorie = null;
 
+    /**
+     * @var Collection<int, Order>
+     */
+    #[ORM\ManyToMany(targetEntity: Order::class, mappedBy: 'videosIndividuales')]
+    private Collection $orders;
+
     public function __construct()
     {
         $this->packs = new ArrayCollection();
+        $this->orders = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -112,6 +119,33 @@ class Video
     public function setCategorie(?Categorie $categorie): static
     {
         $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Order>
+     */
+    public function getOrders(): Collection
+    {
+        return $this->orders;
+    }
+
+    public function addOrder(Order $order): static
+    {
+        if (!$this->orders->contains($order)) {
+            $this->orders->add($order);
+            $order->addVideosIndividuale($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrder(Order $order): static
+    {
+        if ($this->orders->removeElement($order)) {
+            $order->removeVideosIndividuale($this);
+        }
 
         return $this;
     }
