239ce3d56e8cc10227def72ab75e8a233f71bdd8
samantha-eva <samanthathiebaut@gmail.com>
2025-09-03 13:40:17 +0200

    Template page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PackRepository;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(PackRepository $packRepository): Response
     {
-        return $this->render('home/index.html.twig', [
+
+        $packs = $packRepository->findAllPacks();
+
+            return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'packs' => $packs, // <-- on passe les packs à la vue
         ]);
     }
+
+
+
 }
