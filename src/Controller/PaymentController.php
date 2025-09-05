67ccbf3510c0b5c45988d3736671e38d1a4a4263
samantha-eva <samanthathiebaut@gmail.com>
2025-09-05 11:25:21 +0200

    integration stripe

---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Stripe\Stripe;
+use Stripe\PaymentIntent;
+
+class PaymentController extends AbstractController
+{
+    #[Route('/create-payment-intent', name: 'create_payment_intent', methods: ['POST'])]
+    public function createPaymentIntent(Request $request): JsonResponse
+    {
+        Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY']);
+
+        // Exemple: récupérer le montant depuis la requête ou un produit
+        $amount = 2000; // en centimes -> 20.00 EUR
+
+        $paymentIntent = PaymentIntent::create([
+            'amount' => $amount,
+            'currency' => 'eur',
+            'automatic_payment_methods' => [
+                'enabled' => true,
+            ],
+        ]);
+
+        return new JsonResponse([
+            'clientSecret' => $paymentIntent->client_secret,
+        ]);
+    }
+
+    #[Route('/payment', name: 'payment')]
+    public function index(): Response
+    {
+        return $this->render('payment/index.html.twig', [
+            'stripe_public_key' => $_ENV['STRIPE_PUBLIC_KEY'],
+        ]);
+    }
+
+    #[Route('/payment/success', name: 'payment_success')]
+    public function success(): Response
+    {
+        return $this->render('payment/success.html.twig');
+    }
+
+    #[Route('/payment/cancel', name: 'payment_cancel')]
+    public function cancel(): Response
+    {
+        return $this->render('payment/cancel.html.twig');
+    }
+}
